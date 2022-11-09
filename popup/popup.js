4044ad43e95f36ee6f5a2cab9b0061c05315fece
Aristylux <axelmezade@orange.fr>
2022-11-09 19:26:46 +0100

    add switch

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,6 @@
 console.log("load");
 
+/*
 browser.storage.local.get('updateView', function(items){
     assignTextToTextArea(items.updateView);
     //browser.storage.local.remove('updateView');
@@ -9,7 +10,7 @@ browser.storage.local.get('locale', function(items){
     document.getElementById(items.locale).classList.add("active");
     selectLanguage(items.locale);
 });
-
+*/
 
 function assignTextToTextArea(newText){
     document.getElementById('hidden_element').innerHTML = newText;
@@ -29,9 +30,9 @@ function hideContainer(){
     }
 }
 
-document.getElementById("nav_but_discovery").addEventListener("click", controlButtonDiscovery);
-document.getElementById("nav_but_about").addEventListener("click", controlButtonAbout);
-document.getElementById("nav_but_settings").addEventListener("click", controlButtonSettings);
+document.getElementById("nav_but_discovery").addEventListener('click', controlButtonDiscovery);
+document.getElementById("nav_but_about").addEventListener('click', controlButtonAbout);
+document.getElementById("nav_but_settings").addEventListener('click', controlButtonSettings);
 
 function controlButtonDiscovery(){
     hideContainer();
@@ -51,5 +52,21 @@ function controlButtonSettings(){
     document.getElementById("nav_but_settings").classList.add("active");
 }
 
+// ---------------------- Check button ------------
+var checkbox = document.getElementById("checkbox");
+
+checkbox.addEventListener('change', function () {
+  if (checkbox.checked) {
+    // do this
+    // add path to exluded list
+    path = window.location.pathname;
+
+    console.log('Checked');
+  } else {
+    
+    // remove path to exluded list
+    console.log('Not checked');
+  }
+});
 
 console.log("End");
